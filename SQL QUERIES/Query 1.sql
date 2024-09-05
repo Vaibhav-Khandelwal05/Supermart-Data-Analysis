3862d561f33c0a88ec5d05294148b9242b6b97ab
Vaibhav Khandelwal <114105465+Vaibhav-Khandelwal05@users.noreply.github.com>
2024-09-06 01:25:19 +0530

    Update Query 1.sql

--- SQL QUERIES/Query 1.sql
+++ SQL QUERIES/Query 1.sql
@@ -1,3 +1,6 @@
+# List of products with a Base Price greater than 500 and that are featured in promo type 'BOGOF'(Buy One Get One Free).
+# This info will help us identify high-value products that are currently being heavily discounted, which can be used for evaluating pricing and promotion strategies. 
+
 SELECT  distinct(product_name) , base_price , promo_type FROM dim_products AS PROD
 RIGHT JOIN FACT_EVENTS AS FACT
 ON PROD.PRODUCT_CODE = FACT.PRODUCT_CODE
